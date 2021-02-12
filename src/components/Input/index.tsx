412faa9e1e3ccf38849f33f21c4cbc152c7ff69c
Isaac <isaacsvianna@gmail.com>
2021-02-12 08:03:29 -0300

    Update: usability on forms

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useImperativeHandle, forwardRef } from "react";
 import { TextInputProps } from "react-native";
 import { useField } from "@unform/core";
 
@@ -13,23 +13,36 @@ interface InputValueReference {
   value: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref
+) => {
   const inputElementRef = useRef<any>(null);
 
   const { registerField, defaultValue = "", fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
-  
+
+  useImperativeHandle(ref, () => ({ 
+    focus() {
+      inputElementRef.current.focus();
+    }
+   }));
+
   useEffect(() => {
     registerField<string>({
       name: fieldName,
       ref: inputValueRef.current,
       path: "value",
-      setValue(ref: any, value){
+      setValue(ref: any, value) {
         inputValueRef.current.value = value;
-        inputElementRef.current.setNativeProps({text: value});
+        inputElementRef.current.setNativeProps({ text: value });
       },
       clearValue() {
-        inputValueRef.current.value = '';
+        inputValueRef.current.value = "";
         inputElementRef.current.clear();
       },
     });
@@ -39,7 +52,7 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
     <Container>
       <Icon name={icon} size={20} color="#666360" />
       <TextInput
-        ref = {inputElementRef}
+        ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
@@ -52,4 +65,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   );
 };
 
-export default Input;
+export default forwardRef(Input);
